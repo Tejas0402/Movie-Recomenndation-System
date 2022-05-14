15d7c31c7944770793c0aab3b2b6f5fadb5a2c5f
Tejas <tejasgathadi@gmail.com>
2022-05-14 22:26:31 +0530

    html

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='template')
 
 @app.route('/')
-def index():
-    return '<h1> Hello </h1>'
+def home():
+    return render_template('prog.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)  
